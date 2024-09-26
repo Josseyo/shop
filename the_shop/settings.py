9fc9155c1f4d2a1d116be5c37a5150be4389352d
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-09-26 13:37:07 +0000

    completed webhook handler

--- the_shop/settings.py
+++ the_shop/settings.py
@@ -10,13 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
-
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -172,3 +170,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
