8d20b5cf25426dd86b94b2e0b60d1c18684b74b6
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-10-02 11:01:04 +0000

    modified:   the_shop/settings.py

--- the_shop/settings.py
+++ the_shop/settings.py
@@ -160,10 +160,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
