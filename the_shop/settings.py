b22ed14a05a18a6139644a78ff17a1f8ac25d24d
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-10-03 11:50:45 +0000

    add cach control

--- the_shop/settings.py
+++ the_shop/settings.py
@@ -170,7 +170,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-if "USE_AWS" in os.environ:
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # AWS S3 Bucket Config
     AWS_STORAGE_BUCKET_NAME = "theshop-8565bb64956d"
     AWS_S3_REGION_NAME = "us-east-1"
